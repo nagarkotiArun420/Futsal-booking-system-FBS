9f0242bded6be609274bcad9ba6f274bef7eb86c
Arun Nagarkoti <nagarkotiarun420@gmail.com>
2025-09-03 20:29:23 +0545

    log out and login functionality finished

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from futsal_main.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 # Create your views here.
@@ -25,5 +27,24 @@ def register(request):
     return render(request,"register.html",context)
 
 def login(request):
-    
-    return render(request, "login.html")
+    if request.method == 'POST':
+         form = AuthenticationForm(request, request.POST)
+         if form.is_valid():
+           
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect('home')   
+            else: 
+                print(form.errors)         
+    else:
+        form = AuthenticationForm()
+   
+    return render(request, "login.html",{'form':form})
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
